19f619c7e07dbdbae47987acc1cfacbc92d45c31
brn <brn.gdsn@gmail.com>
2025-02-22 13:48:27 -0500

    Auto commit on change

--- api/index.py
+++ api/index.py
@@ -4,21 +4,8 @@ import sys
 import platform
 import os
 
-import asyncio
-import traceback
-from readmeai.config.settings import (
-    AppConfig,
-    AppConfigModel,
-    ConfigHelper,
-    GitConfig,
-    load_config,
-    load_config_helper,
-)
-from readmeai.core import logger, model, preprocess
-from readmeai.markdown import headers, tables, tree
-from readmeai.services import version_control as vcs
-
-logger = logger.Logger(__name__)
+from readmeai.cli import options
+from readmeai.main import main
 
 def get_response_content(readme_content):
     return {
@@ -72,97 +59,19 @@ def get_response_content(readme_content):
             }
         }
 
-async def readme_agent(conf: AppConfig, conf_helper: ConfigHelper) -> None:
-    """Orchestrates the README file generation process."""
-    logger.info("README-AI is now executing.")
-    logger.info(f"User Repository: {conf.git.repository}")
-    logger.info(f"Output File: {conf.paths.output}")
-    logger.info(f"Model Engine:  {conf.api.model}")
-
-    name = conf.git.name
-    placeholder = conf.md.default
-    repository = conf.git.repository
-
-    llm = model.OpenAIHandler(conf)
-
-    try:
-        temp_dir = vcs.clone_repo_to_temp_dir(repository)
-        tree_generator = tree.TreeGenerator(
-            conf_helper,
-            temp_dir,
-            repository,
-            project_name="readmeai",
-            max_depth=3,
-        )
-        tree_str = tree_generator.generate_and_format_tree()
-        conf.md.tree = conf.md.tree.format(tree_str)
-        logger.info(f"Repository tree: {conf.md.tree}")
-
-        parser = preprocess.RepositoryParser(conf, conf_helper)
-        dependencies, files = parser.get_dependencies(temp_dir)
-        logger.info(f"Dependencies: {dependencies}")
-        logger.info(f"Files: {files}")
-
-        # Generate codebase file summaries and README.md text via LLMs.
-        if conf.cli.offline is False:
-            code_summary = await llm.code_to_text(
-                conf_helper.ignore_files,
-                files,
-                conf.prompts.code_summary,
-            )
-            logger.info(f"Code summaries returned:\n{code_summary[:5]}")
-            prompts = [
-                conf.prompts.slogan.format(conf.git.name),
-                conf.prompts.overview.format(repository, code_summary),
-                conf.prompts.features.format(repository, tree),
-            ]
-            slogan, overview, features = await llm.chat_to_text(prompts)
-
-        else:
-            conf.md.tables = tables.build_recursive_tables(
-                repository, temp_dir, placeholder
-            )
-            code_summary = placeholder
-            slogan, overview, features = (
-                conf.md.default,
-                conf.md.default,
-                conf.md.default,
-            )
-
-        # Generate README.md file.
-        conf.prompts.slogan = slogan
-        conf.md.header = conf.md.header.format(name.upper(), slogan)
-        conf.md.intro = conf.md.intro.format(overview, features)
-        headers.build_readme_md(conf, conf_helper, dependencies, code_summary)
-
-    except Exception as excinfo:
-        logger.error(
-            f"Exception: {excinfo}\nStacktrace: {traceback.format_exc()}"
-        )
-    finally:
-        await llm.close()
-
-    logger.info("README-AI execution complete.")
-
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         readme_content = 'n/a'
-
-        conf = load_config()
-        conf_model = AppConfigModel(app=conf)
-        conf_helper = load_config_helper(conf_model)
-        conf.api.model = 'gpt-4o-mini'
-        conf.cli.badges = True
-        conf.cli.emojis = True
-        conf.api.model = 'gpt-4o-mini'
-        conf.cli.offline = True
-        conf.paths.output = 'README.md'
-        conf.api.temperature = 0.7
-        conf.git = GitConfig(repository='https://github.com/brngdsn/unmd', name=None)
-        conf.git.name = vcs.get_user_repository_name('https://github.com/brngdsn/unmd')[1]
-        logger.info(f"Configuration: {conf.git}")
-        asyncio.run(readme_agent(conf, conf_helper))
-
+        main(
+            # api_key="YOUR_API_KEY",
+            # badges="desired_badge_options",
+            emojis=True,
+            offline=True,
+            # model="model_name",
+            output="README.md",
+            repository="https://github.com/brngdsn/unmd",
+            temperature=0.7,
+        )
         self.send_response(200)
         self.send_header('Content-Type', 'application/json')
         self.end_headers()
