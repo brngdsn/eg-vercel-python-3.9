5c51f5a30648dd1131ea9e1c6c53084e9aa93a8a
brn <brn.gdsn@gmail.com>
2025-02-22 10:00:56 -0500

    try

--- api/index.py
+++ api/index.py
@@ -7,5 +7,9 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-Type', 'application/json')
         self.end_headers()
-        response_content = {'message': '(P)ython(API)', 'info': readmeai.__version__}
+        response_content = {
+            'message': '(P)ython(API)',
+            'info': readmeai.__version__,
+            'meta': readmeai.__spec__
+        }
         self.wfile.write(json.dumps(response_content).encode())
